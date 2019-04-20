55d42ae320da601a935390cff88ed085be83de20
mahuihui0315 <mhhmhhmhh@outlook.com>
2019-04-20 22:13:10 +0800

    part of decorator demo

---
+++ decorator/ShapeDecorator.java
@@ -0,0 +1,13 @@
+package DecoratorPattern;
+
+public abstract class ShapeDecorator implements Shape {
+    protected Shape decoratorShape;
+
+    public ShapeDecorator(Shape decoratorShape){
+        this.decoratorShape=decoratorShape;
+    }
+
+    public void draw(){
+        decoratorShape.draw();
+    }
+}
